6398ef9c2cc1608e0c7252f0590bece63d31d80b
SakshamSinghSaloni <sakshamsailoni89@gmail.com>
2026-09-09 22:03:45 +0530

    Remove main() from scheduler implementations

--- scheduling/schedulers/PRIORITY_NP.c
+++ scheduling/schedulers/PRIORITY_NP.c
@@ -1,8 +1,4 @@
-// Non-Preemptive Priority Scheduling Algorithm
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "../include/scheduler_utilities.h"
+#include "../include/schedulers.h"
 
 void PRIORITY_NP(Process p[], int N){
     
@@ -38,46 +34,4 @@ void PRIORITY_NP(Process p[], int N){
             is_running = 0;
         }
     }
-}
-
-int main(){
-
-    int N;
-    printf("\nEnter the total number of processes: ");
-    scanf("%d", &N);
-
-    Process *p = malloc(sizeof(Process) * N);
-    if (p == NULL) return 1;
-
-    for (size_t i = 0; i < N; i++){
-        p[i].PID = i;
-        printf("\nP%d\n", i);
-        printf("Arrival time = ");
-        scanf("%d", &p[i].AT);
-        printf("Burst Time = ");
-        scanf("%d", &p[i].BT);
-        printf("Priority = ");
-        scanf("%d", &p[i].PRIORITY);
-        p[i].CT = p[i].TAT = p[i].WT = 0;
-    }
-
-    PRIORITY_NP(p, N);
-
-    int total_tat = 0;
-    int total_wt = 0;
-
-    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\tPRIORITY\n");
-    for (size_t i = 0; i < N; i++){
-        total_tat += p[i].TAT;
-        total_wt += p[i].WT;
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].PID, p[i].AT, p[i].BT, p[i].CT, p[i].TAT, p[i].WT, p[i].PRIORITY);
-    }
-    
-    float avg_tat = (float)total_tat / N;
-    float avg_wt = (float)total_wt / N;
-
-    printf("\nAverage TAT = %.2f", avg_tat);
-    printf("\nAverage WT = %.2f", avg_wt);
-
-    free(p);
 }
--- scheduling/schedulers/SRTF.c
+++ scheduling/schedulers/SRTF.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "../include/scheduler_utilities.h"
+#include "../include/schedulers.h"
 
 void SRTF(Process p[], int N){
 
@@ -55,42 +53,4 @@ void SRTF(Process p[], int N){
             is_running = 0;
         }
     }
-}
-
-int main(){
-
-    int N;
-    printf("\nEnter the total number of processes: ");
-    scanf("%d", &N);
-
-    Process *p = malloc(sizeof(Process) * N);
-    if (p == NULL) return 1;
-
-    for (size_t i = 0; i < N; i++){
-        p[i].PID = i;
-        printf("\nP%d\n", i);
-        printf("Arrival time = ");
-        scanf("%d", &p[i].AT);
-        printf("Burst Time = ");
-        scanf("%d", &p[i].BT);
-        p[i].CT = p[i].TAT = p[i].WT = p[i].PRIORITY = 0;
-    }
-
-    SRTF(p, N);
-
-    int total_tat = 0;
-    int total_wt = 0;
-
-    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
-    for (size_t i = 0; i < N; i++){
-        total_tat += p[i].TAT;
-        total_wt += p[i].WT;
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", p[i].PID, p[i].AT, p[i].BT, p[i].CT, p[i].TAT, p[i].WT);
-    }
-    
-    float avg_tat = (float)total_tat / N;
-    float avg_wt = (float)total_wt / N;
-
-    printf("\nAverage TAT = %.2f", avg_tat);
-    printf("\nAverage WT = %.2f", avg_wt);
 }
