5d1b322c10662e12049bfea2d178d4d5e8a82ad9
SakshamSinghSaloni <sakshamsailoni89@gmail.com>
2026-09-12 18:12:43 +0530

    cleanup

--- scheduling/include/schedulers.h
+++ scheduling/include/schedulers.h
@@ -5,10 +5,10 @@
 #include "process.h"
 
 void FCFS(Process p[], int N);
+void SJF(Process p[], int N);
+void SRTF(Process p[], int N);
 void PRIORITY_NP(Process p[], int N);
 void PRIORITY_P(Process p[], int N);
 void ROUND_ROBIN(Process p[], int N, int quanta);
-void SJF(Process p[], int N);
-void SRTF(Process p[], int N);
 
 #endif
